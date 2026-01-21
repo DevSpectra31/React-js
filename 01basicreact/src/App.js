fba464e4114722131523bad0947fe85b7deb699c
Er. Vineet Rajpal <rajpalvineet800@gmail.com>
2026-01-21 12:35:50 +0530

    leanred difference about vite and cra

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+   <h1>Let hands dirty with react</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+   <h1>Get hand dirty with vite for react</h1>
+  )
+}
+
+export default App
