b321f807856ab274bd352e57da844e8a254f9f1e
Er. Vineet Rajpal <rajpalvineet800@gmail.com>
2026-05-01 23:23:05 +0530

    start revision of reactjs

---
+++ components/README.md
@@ -0,0 +1,3 @@
+# What is a Component based Architecture
+# make multiple functions and use again and again.
+# component is a piece of code and that is used for a particular feature in your project.
---
+++ components/src/component/Card.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Card() {
+    const user = 'Vineet'
+  return (
+    <div>
+      <div className='card'>
+        <h1>Hello guys I am {user}</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam, dignissimos.
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default Card
