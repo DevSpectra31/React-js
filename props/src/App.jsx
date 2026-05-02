c4d026aca0ace3d6c28203362a5c507b0fb2ec55
Er. Vineet Rajpal <rajpalvineet800@gmail.com>
2026-05-02 15:26:39 +0530

    get to know about prop drilling

--- props/src/App.jsx
+++ props/src/App.jsx
@@ -4,7 +4,9 @@ function App() {
   return (
     <div className='parent'>
       <div className='card'>
-      <h1>Vineet Rajpal</h1>
+        <h1>Vineet Rajpal</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum rem quam molestiae!</p>
+        <button>view profile</button>
       </div>
     </div>
   )
